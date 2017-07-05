9ef2684e08ae09582adf0bd4b67911f2795b28dc
dariofg93 <dariofg93@gmail.com>
2017-07-05 17:08:26 -0300

    cambio de ip

--- app/src/main/java/com/uis/carmensandiego/carmensandiego/service/Connection.java
+++ app/src/main/java/com/uis/carmensandiego/carmensandiego/service/Connection.java
@@ -5,8 +5,8 @@ import retrofit.RestAdapter;
 public class Connection {
 
     public static CarmenSanDiegoService getService() {
-        String SERVER_IP = "192.168.1.34"; //esta ip se usa para comunicarse con mi localhost en el emulador de Android Studio
-        String SERVER_IP_GENY = "192.168.1.40";//esta ip se usa para comunicarse con mi localhost en el emulador de Genymotion
+        String SERVER_IP = "10.12.1.121"; //esta ip se usa para comunicarse con mi localhost en el emulador de Android Studio
+        //String SERVER_IP_GENY = "192.168.1.40";//esta ip se usa para comunicarse con mi localhost en el emulador de Genymotion
         String API_URL = "http://"+ SERVER_IP +":9000";
 
         RestAdapter restAdapter = new RestAdapter.Builder().setEndpoint(API_URL).build();
